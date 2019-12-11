a36ff7aeb77e5edccb463cf0d7a598c03277c9b8
Utwo <mihai.legat@gmail.com>
2019-12-11 15:25:56 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const APPLICATION_TYPE = "application/javascript; charset=utf-8";
 
-export const CACHE_CONTROL = "max-age=3600";
+export const CACHE_CONTROL = "public,max-age=3600";
 
 export const RESPONSE_TYPE = "application/javascript";
